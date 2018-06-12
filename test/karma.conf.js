c6bca06796e52e8a2f1a6f46c5471a6fe0323581
William de Branco <williamgbranco@gmail.com>
2018-06-12 02:11:18 -0300

    Add jquery-ui to bower dependencies.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-material/angular-material.js',
+      'bower_components/jquery-ui/jquery-ui.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',

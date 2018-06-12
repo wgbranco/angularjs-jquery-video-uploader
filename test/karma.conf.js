03f83fb590f89c9c1ad440a3a5e174ce7e90f99f
William de Branco <williamgbranco@gmail.com>
2018-06-12 02:48:43 -0300

    Fix scripts order.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,11 +28,11 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/blueimp-file-upload/js/jquery.fileupload.js',
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/jquery-ui/jquery-ui.js',
+      'bower_components/blueimp-file-upload/js/jquery.fileupload.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
